fe3f95e569b61ddbd18bad5a2930115988eefcdc
Zac Spitzer <zac.spitzer@gmail.com>
2015-10-12 12:08:01 +1100

    Update internal.js
    there is no need to clone() here,  it creates a memory leak as it's not attached the to DOM and therefore isn't removed by remove()

--- src/internal.js
+++ src/internal.js
@@ -873,7 +873,7 @@ function replacePlaceHolder(placeholder, element)
     placeholder.each(function (index, item)
     {
         // todo: check how append is implemented. Perhaps cloning here is superfluous.
-        $(item).before(element.clone(true)).remove();
+        $(item).before(element).remove();
     });
 }
 
